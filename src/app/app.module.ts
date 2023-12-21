e34713560459cfe41abd27aeb4a113ef777a1d90
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-12-21 15:38:44 +0100

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { RegisterComponent } from './register/register.component';
 import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
 import { SendEmailComponent } from './send-email/send-email.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { ResetPasswordComponent } from './reset-password/reset-password.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"dabubble-97d36","appId":"1:7321163189:web:ccc6f61096f18ad9fab5fc","storageBucket":"dabubble-97d36.appspot.com","apiKey":"AIzaSyB7yQy3kZNdO3QSJUnhQFmfCWHsVsc0sPo","authDomain":"dabubble-97d36.firebaseapp.com","messagingSenderId":"7321163189"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging())
   ],
   providers: [],
   bootstrap: [AppComponent]
