f097841bd129b2b7c15b5f97154dd5ab8d67a06a
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-12-21 06:46:57 +0100

    add firebase

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyB7yQy3kZNdO3QSJUnhQFmfCWHsVsc0sPo",
+        authDomain: "dabubble-97d36.firebaseapp.com",
+        projectId: "dabubble-97d36",
+        storageBucket: "dabubble-97d36.appspot.com",
+        messagingSenderId: "7321163189",
+        appId: "1:7321163189:web:ccc6f61096f18ad9fab5fc"
+    }
+  };
+  
