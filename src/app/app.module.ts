f1a71d4274957c72afd5c6ec7ac7b0fb642b91af
AhmedBenJaballah <127104159+AhmedBenJaballah@users.noreply.github.com>
2023-12-21 14:38:48 +0100

    added start screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
+import { SendEmailComponent } from './send-email/send-email.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChooseAvatarComponent,
+    SendEmailComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
