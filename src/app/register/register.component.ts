f41dd6966d99a6219c3c2adfa362be3ffb620f1c
AhmedBenJaballah <127104159+AhmedBenJaballah@users.noreply.github.com>
2023-12-23 13:13:49 +0100

    style validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,14 +10,44 @@ import { User } from '../models/User.class';
 })
 export class RegisterComponent {
 
-  user:User= new User()
+  user:User= new User();
+  privacyPolicyChecked!:boolean;
+  disableForm:boolean=false;
 
   constructor(
     public storageService: StorageService,
     private userService: UserService
   ) {}
 
+  calculatePasswordStrength(password: string): string {
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>=]/.test(password);
+  
+    if (password.length < 6 || !((hasLowerCase || hasUpperCase) && hasSpecialChar)) {
+      return 'Schwach';
+    } else if (password.length < 8) {
+      return 'Mittel';
+    } else {
+      return 'Stark';
+    }
+  }
+  
+
+  isWeakPassword(): boolean {
+    return this.calculatePasswordStrength(this.user.password) === 'Schwach';
+  }
+
+  isMediumPassword(): boolean {
+    return this.calculatePasswordStrength(this.user.password) === 'Mittel';
+  }
+
+  isStrongPassword(): boolean {
+    return this.calculatePasswordStrength(this.user.password) === 'Stark';
+  }
+
   async register() {
+    this.disableForm=true;
     const name = (document.getElementById('textRegister') as HTMLInputElement).value;
     const email = (document.getElementById('emailRegister') as HTMLInputElement).value;
     const password = (document.getElementById('passRegister') as HTMLInputElement).value;
@@ -29,8 +59,9 @@ export class RegisterComponent {
     }
 
     try {
-      this.storageService.plusStep();
       await this.userService.registerUser({ name, email, password });
+      this.storageService.plusStep();
+      this.disableForm=false;
     } catch (error) {
       console.error('Registrierungsfehler', error);
     }
