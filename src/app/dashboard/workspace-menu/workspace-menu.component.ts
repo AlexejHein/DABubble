c71126431b52644f77b2972b4efe03ca4e594564
Marcel Zilkowski <info@internetaces.com>
2023-12-25 14:46:02 +0100

    Dropdown menus added

--- src/app/dashboard/workspace-menu/workspace-menu.component.ts
+++ src/app/dashboard/workspace-menu/workspace-menu.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-workspace-menu',
   templateUrl: './workspace-menu.component.html',
   styleUrls: ['./workspace-menu.component.scss']
 })
-export class WorkspaceMenuComponent {
+export class WorkspaceMenuComponent implements OnInit{
+userListState = true;
+channelListState = true;
+userListClass = "open";
+channelListClass = "open";
+
+ngOnInit(){
+}
+
+  openUserList() {
+    if(this.userListState) {
+      this.userListState = false;
+      this.userListClass = "close";
+    }
+    else {
+      this.userListState = true;
+      this.userListClass = "open";
+    }
+  }
+
+  openChannelList() {
+    if(this.channelListState) {
+      this.channelListState = false;
+      this.channelListClass = "close";
+    }
+    else {
+      this.channelListState = true;
+      this.channelListClass = "open";
+    }
+  }
+ 
 
 }
