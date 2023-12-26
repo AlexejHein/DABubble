3378d210a2532cf898e4f0091bd73334920c1101
Marcel Zilkowski <info@internetaces.com>
2023-12-26 23:05:54 +0100

    thread list added

--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Thread } from '../models/thread.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThreadsService {
-
+  
   constructor() { }
-}
+
+  setSelectedThread(thread: Thread | null) {
+   // this.selectedThreadSubject.next(thread);
+  }
+
+} 
