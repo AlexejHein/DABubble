6e52856528915ea7dd9009bec44155286b8ec1b7
Marcel Zilkowski <info@internetaces.com>
2023-12-26 23:06:15 +0100

    tread list added

--- src/app/dashboard/thread-list/thread-list.component.ts
+++ src/app/dashboard/thread-list/thread-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Thread } from 'src/app/models/thread.class';
+import { ThreadsService } from 'src/app/services/threads.service';
 
 @Component({
   selector: 'app-thread-list',
   templateUrl: './thread-list.component.html',
   styleUrls: ['./thread-list.component.scss']
 })
-export class ThreadListComponent {
+export class ThreadListComponent implements OnInit {
+
+  firestore: Firestore = inject(Firestore)
+  items$!: Observable<any[]>;
+  allThreads: any[] = [];
+  thread = new Thread();
+
+  constructor(  protected threadsService: ThreadsService,) {}
+
+  ngOnInit(): void {
+    const aCollection = collection(this.firestore, 'threads')		
+    this.items$ = collectionData(aCollection, { idField: 'id' });	
+    this.items$.subscribe((threads) => { 
+      this.allThreads = threads;
+      console.log(threads);
+      });
+  }
+
+  onThreadClick(selectedThread: Thread): void {
+    this.threadsService.setSelectedThread(selectedThread);
+    console.log("Selected Thread:", selectedThread);
+  }
 
 }
