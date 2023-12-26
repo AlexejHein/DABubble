1d4be99e65247e78ba28f5de72bd72920937f991
Marcel Zilkowski <info@internetaces.com>
2023-12-26 19:23:42 +0100

    Thread list component created

---
+++ src/app/dashboard/thread-list/thread-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thread-list',
+  templateUrl: './thread-list.component.html',
+  styleUrls: ['./thread-list.component.scss']
+})
+export class ThreadListComponent {
+
+}
