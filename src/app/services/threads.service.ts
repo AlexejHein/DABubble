03304944363963c5c668e868e931dc11ec8c90c8
Marcel Zilkowski <info@internetaces.com>
2023-12-27 10:09:27 +0100

    selected thread title added to dashboard

--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Thread } from '../models/thread.class';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThreadsService {
-  
+  private selectedThreadSubject = new BehaviorSubject<Thread | null>(null);
+  selectedThread = this.selectedThreadSubject.asObservable();
+
   constructor() { }
 
   setSelectedThread(thread: Thread | null) {
-   // this.selectedThreadSubject.next(thread);
+   this.selectedThreadSubject.next(thread);
   }
 
 } 
