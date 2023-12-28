dc115529d9dbec4975b55e8904f58e7ec96b90bd
Marcel Zilkowski <info@internetaces.com>
2023-12-28 09:56:00 +0100

    thread author infos added

--- src/app/models/thread.class.ts
+++ src/app/models/thread.class.ts
@@ -1,5 +1,7 @@
 export class Thread {
     id: string;
+    authorName: string | null;
+    authorId: string | null;
     title: string;
     description: string;
     createdAt: Date;
@@ -8,12 +10,16 @@ export class Thread {
     constructor(obj?: any) {
         if (obj) {
             this.id = obj.id ?? '';
+            this.authorName = obj.authorName ?? '';
+            this.authorId = obj.authorId ?? '';
             this.title = obj.title ?? '';
             this.description = obj.description ?? '';
             this.createdAt = obj.createdAt ?? new Date();
             this.updatedAt = obj.updatedAt ?? new Date();
         } else {
             this.id = '';
+            this.authorName = '';
+            this.authorId = '';
             this.title = '';
             this.description = '';
             this.createdAt = new Date();
@@ -24,6 +30,8 @@ export class Thread {
     public toJSON(): any {
         return {
             id: this.id,
+            authorName: this.authorName,
+            authorId: this.authorId,
             title: this.title,
             description: this.description,
             createdAt: this.createdAt,
