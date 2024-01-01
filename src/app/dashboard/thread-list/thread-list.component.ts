9e6fbb4b9b19a343283c00c25b4693be76ec41a4
Marcel Zilkowski <info@internetaces.com>
2024-01-01 20:23:01 +0100

    Added list of threads to dashboard

--- src/app/dashboard/thread-list/thread-list.component.ts
+++ src/app/dashboard/thread-list/thread-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Thread } from 'src/app/models/thread.class';
+import { ThreadsService } from 'src/app/services/threads.service';
 
 @Component({
   selector: 'app-thread-list',
   templateUrl: './thread-list.component.html',
   styleUrls: ['./thread-list.component.scss']
 })
-export class ThreadListComponent {
+export class ThreadListComponent implements OnInit {
+
+  firestore: Firestore = inject(Firestore)
+  items$!: Observable<any[]>;
+  allThreads: any[] = [];
+  thread = new Thread();
+  threadId: any;
+
+  constructor(  protected threadsService: ThreadsService,) {}
+
+  ngOnInit(): void {
+    const aCollection = collection(this.firestore, 'threads')		
+    this.items$ = collectionData(aCollection, { idField: 'id' });	
+    this.items$.subscribe((threads) => { 
+      this.allThreads = threads;
+      console.log(threads);
+      });
+  }
+
+  onThreadClick(selectedThread: Thread): void {
+    this.threadsService.setSelectedThread(selectedThread);
+    console.log("Selected Thread:", selectedThread);
+    console.log("Selected Thread ID:", selectedThread.id);
+    this.threadId = selectedThread.id;
+  }
 
 }
--- src/app/services/threads.service.ts
+++ src/app/services/threads.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Channel } from '../models/channel.class';
+import { Thread } from '../models/thread.class';
 import {BehaviorSubject} from "rxjs";
 
 @Injectable({
@@ -9,10 +10,17 @@ export class ThreadsService {
   private selectedChannelSubject = new BehaviorSubject<Channel | null>(null);
   selectedChannel = this.selectedChannelSubject.asObservable();
 
+  private selectedThreadSubject = new BehaviorSubject<Thread | null>(null);
+  selectedThread = this.selectedThreadSubject.asObservable();
+
   constructor() { }
 
   setSelectedChannel(channel: Channel | null) {
    this.selectedChannelSubject.next(channel);
   }
+
+  setSelectedThread(thread: Thread | null) {
+    this.selectedThreadSubject.next(thread);
+   }
 
 } 
