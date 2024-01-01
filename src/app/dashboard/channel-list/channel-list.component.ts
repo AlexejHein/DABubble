2e8875c67aee9ec73f6e834dad233289a9dc9f3b
Marcel Zilkowski <info@internetaces.com>
2024-01-01 10:49:08 +0100

    changed thread to channel

---
+++ src/app/dashboard/channel-list/channel-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Channel } from 'src/app/models/channel.class';
+import { ThreadsService } from 'src/app/services/threads.service';
+
+@Component({
+  selector: 'app-channel-list',
+  templateUrl: './channel-list.component.html',
+  styleUrls: ['./channel-list.component.scss']
+})
+export class ChannelListComponent implements OnInit {
+
+  firestore: Firestore = inject(Firestore)
+  items$!: Observable<any[]>;
+  allChannels: any[] = [];
+  channel = new Channel();
+  channelId: any;
+
+  constructor(  protected threadsService: ThreadsService,) {}
+
+  ngOnInit(): void {
+    const aCollection = collection(this.firestore, 'threads')		
+    this.items$ = collectionData(aCollection, { idField: 'id' });	
+    this.items$.subscribe((channels) => { 
+      this.allChannels = channels;
+      console.log(channels);
+      });
+  }
+
+  onThreadClick(selectedChannel: Channel): void {
+    this.threadsService.setSelectedChannel(selectedChannel);
+    console.log("Selected Thread:", selectedChannel);
+    console.log("Selected Thread ID:", selectedChannel.id);
+    this.channelId = selectedChannel.id;
+  }
+
+}
