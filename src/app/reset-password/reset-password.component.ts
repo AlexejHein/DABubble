e0003e1fa6baa3d98b7d407d52d38ba1b8c469a8
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-01-08 19:59:54 +0100

    finish pw reset

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { StorageService } from '../services/storage.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-reset-password',
@@ -7,5 +9,34 @@ import { StorageService } from '../services/storage.service';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent {
-  constructor( public storageService:StorageService){}
+  newPassword: string = '';
+  confirmPassword: string = '';
+  code: string = '';
+  email: string = '';
+
+  constructor(
+    public storageService: StorageService,
+    private auth: AngularFireAuth,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.code = params['oobCode'];
+      this.email = params['email'];
+    });
+  }
+  async resetPassword() {
+    if (this.newPassword !== this.confirmPassword) {
+      alert("Passwörter stimmen nicht überein!");
+      return;
+    }
+
+    try {
+      await this.auth.confirmPasswordReset(this.code, this.newPassword);
+      await this.router.navigate(['']);
+    } catch (error) {
+      console.error("Fehler beim Zurücksetzen des Passworts", error);
+    }
+  }
 }
+
