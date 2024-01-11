16adca403d12686e1a8e9a782539892070661a16
Marcel Zilkowski <info@internetaces.com>
2024-01-11 22:56:23 +0100

    add and remove user from channel

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { Firestore, addDoc, collection, collectionData, doc, updateDoc, arrayUnion, arrayRemove } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, doc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 import { Channel } from '../models/channel.class';
+import { UserService } from '../services/user.service';
+import { User } from '../models/User.class';
+import { Subscription } from "rxjs";
+import { ThreadsService } from '../services/threads.service';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -10,23 +15,99 @@ import { Channel } from '../models/channel.class';
 })
 export class DialogAddUserComponent implements OnInit {
   firestore: Firestore = inject(Firestore);
+  private subscription: Subscription | null = null;
   channel: Channel = new Channel();
   channelId:any = '';
+  selectedChannel: Channel | null = null;
+  channelUsers: any[] = [];
+  selectedUser:  User | null = null;
+  selectedUserId: any = "";
+  allUsers: User[] = [];
+  filteredAllUsers: any[] = [];
+  items$!: Observable<any[]>;
+  currentUserId: string = "";
+  channelUserId: any = "";
+  loadChannelUsers: any[] = [];
+  selUserId: any;
 
-  constructor() {
+  constructor(private userService: UserService,
+    public dialog: MatDialog,
+    private threadsService: ThreadsService) {
+      this.filteredAllUsers = this.allUsers;
 
+    }
+
+  async ngOnInit(): Promise<void> {
+
+    this.subscription = this.threadsService.selectedChannel.subscribe(channel => {
+      this.selectedChannel = channel;
+      this.selectedUser = null;
+      this.channelUsers = this.selectedChannel!.users;
+      this.channelId = this.selectedChannel!.id;
+      this.loadChannelUsers = this.selectedChannel!.users;
+      console.log('selected channel list of users: ', this.loadChannelUsers);
+    });
+
+    this.userService.getUsers().subscribe(users => {
+      this.allUsers = users;
+    });
+
+    const aCollection = collection(this.firestore, 'users');
+    this.items$ = collectionData(aCollection, { idField: 'id' });
+
+    this.items$.subscribe((users) => {
+      this.userService.getCurrentUserId().then((id) => {
+        if (id != null) {
+          this.currentUserId = id;
+          this.userService.setUserOnline(id, true).then(r => {}); // assuming such a method exists
+        }
+        this.allUsers = [
+          users.find(user => user.id === this.currentUserId),
+          ...users.filter(user => user.id !== this.currentUserId)
+        ].filter(Boolean);
+      });
+    });
   }
 
-  ngOnInit(): void {
-
+filterResults(text: string) {
+  if (!text) {
+    this.filteredAllUsers = this.allUsers;
+    return;
   }
 
-  
-  saveUserToChannel() {
-    //const coll = doc(this.firestore, 'channels', this.channelId);
-   // updateDoc(coll, {users: this.channel.users}).then(r => console.log(r));
-
-
+  this.filteredAllUsers = this.allUsers.filter(
+    allUsers => allUsers?.name.toLowerCase().includes(text.toLowerCase())
+  );
 }
+
+showUserInChannel(usersId:any){
+  let indexChecked = this.channelUsers.indexOf(usersId);
+  if(indexChecked > -1){
+  return true;
+  }
+  else {
+    return false;
+  }
+}
+
+addUserToChannel(usersId:any) {
+  //const coll = doc(this.firestore, 'channels', this.channelId);
+ // updateDoc(coll, {users: this.channel.users}).then(r => console.log(r));
+ this.loadChannelUsers.push(usersId);
+ console.log('neue channelUsers:',  this.loadChannelUsers);
+}
+
+removeUserFromChannel(usersId:any) {
+  let channelUserIndex = this.loadChannelUsers.indexOf(usersId);
+  this.loadChannelUsers.splice(channelUserIndex, 1);
+  console.log('neue channelUsers:',  this.loadChannelUsers);
+}
+
+saveUsersToChannel() {
+  const coll = doc(this.firestore, 'channels', this.channelId);
+  this.channel.users = this.loadChannelUsers;
+    updateDoc(coll, {users: this.channel.users}).then(r => console.log(r));
+}
+
 
 }
