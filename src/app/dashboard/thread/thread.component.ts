3e5370416532023780c3ab9f222a42579ee80dfe
Marcel Zilkowski <info@internetaces.com>
2024-01-22 01:56:34 +0100

    load messages by selected thread

--- src/app/dashboard/thread/thread.component.ts
+++ src/app/dashboard/thread/thread.component.ts
@@ -1,11 +1,13 @@
-import {Component, OnInit, inject, signal, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, inject, signal, ChangeDetectorRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable, Subscription } from 'rxjs';
 import { Thread } from 'src/app/models/thread.class';
-import { ThreadsService } from 'src/app/services/threads.service';
+import { ThreadsService } from '../../services/threads.service';
 import { UserService } from '../../services/user.service';
 import { User} from "../../models/User.class";
-import {Reaction} from "../../models/reaction.class";
+import { MessagesService } from "../../services/messages.service";
+import { Message } from "../../models/message.class";
+import { Reaction } from "../../models/reaction.class";
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 @Component({
@@ -30,38 +32,55 @@ export class ThreadComponent implements OnInit {
   currentUserAvatar: string | undefined = "";
   user: string | undefined | null = "";
   allUsers: User[] = [];
+  messages: Message[] = [];
+  allMessages: any[] = [];
+  message: any = {};
+  messageId: any;
+  threadMessages: any[] = [];
  
   constructor(  private userService: UserService,
     protected threadsService: ThreadsService,
+    private messagesService: MessagesService,
     private changeDetector: ChangeDetectorRef) {
 }
 
 
 ngOnInit(): void {
-
   this.subscription = this.threadsService.selectedThread.subscribe(thread => {
     if (thread) {
       this.selectedThread = thread;
       console.log("selected thread in sidebar: ",this.selectedThread);
+      this.threadMessages = this.selectedThread.messages;
+      console.log('messages in thread: ',this.selectedThread.messages);
     }
   });
 
-  const aCollection = collection(this.firestore, 'threads')		
-  this.items$ = collectionData(aCollection, { idField: 'id' });	
+  const threadCollection = collection(this.firestore, 'threads')		
+  this.items$ = collectionData(threadCollection, { idField: 'id' });	
   this.items$.subscribe((threads) => { 
     this.allThreads = threads;
+    });
+
+    const messageCollection = collection(this.firestore, 'messages')		
+  this.items$ = collectionData(messageCollection, { idField: 'id' });	
+  this.items$.subscribe((messages) => { 
+    this.allMessages = messages;
     });
 
 }
 
 selectCurrentThread(threadId:any){
-  console.log('this thread id: ', this.selectedThread);
-if(threadId === this.selectedThread){
+if(threadId === this.selectedThread.id){
   return true;
 }
 else {
   return false;
 }
+}
+
+messageSelectedToThread(messageId:any){
+  let indexChecked = this.threadMessages.indexOf(messageId);
+  return indexChecked > -1;
 }
 
 
