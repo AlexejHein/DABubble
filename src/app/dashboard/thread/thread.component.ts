241572ac6a22abac73ef5df74663b88973277c1d
Marcel Zilkowski <info@internetaces.com>
2024-01-24 08:47:28 +0100

    Write messages in thread

--- src/app/dashboard/thread/thread.component.ts
+++ src/app/dashboard/thread/thread.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, inject, signal, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable, Subscription } from 'rxjs';
 import { Thread } from 'src/app/models/thread.class';
@@ -8,7 +8,9 @@ import { User} from "../../models/User.class";
 import { MessagesService } from "../../services/messages.service";
 import { Message } from "../../models/message.class";
 import { Reaction } from "../../models/reaction.class";
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, updateDoc } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { arrayUnion } from 'firebase/firestore';
 
 @Component({
   selector: 'app-thread',
@@ -27,31 +29,55 @@ export class ThreadComponent implements OnInit {
   thread = new Thread();
   threadId: any;
   selectedThreadId: any;
+  selectedThreadAuthorId: any;
   currentUser: User | null | undefined;
   currentUserId: string | null = "";
   currentUserAvatar: string | undefined = "";
   user: string | undefined | null = "";
   allUsers: User[] = [];
+  selectedUser:  User | null = null;
   messages: Message[] = [];
   allMessages: any[] = [];
   message: any = {};
   messageId: any;
   threadMessages: any[] = [];
+  @ViewChild('myScrollContainer') private myScrollContainer: ElementRef | undefined;
+  showReactions = false;
+  hoveredIndex:any;
+  @ViewChild('messageInput') messageInput: ElementRef | undefined;
+  uploadedFileInfo: any;
+  tooltipVisible = false;
+  tooltipVisibleMap = new Map<string, boolean>();
  
   constructor(  private userService: UserService,
     protected threadsService: ThreadsService,
     private messagesService: MessagesService,
-    private changeDetector: ChangeDetectorRef) {
+    private changeDetector: ChangeDetectorRef,
+    private fireStorage: AngularFireStorage) {
 }
 
 
 ngOnInit(): void {
+  this.userService.getUsers().subscribe(users => {
+    this.allUsers = users;
+  });
+  this.userService.getCurrentUserId().then(id => {
+    this.currentUserId = id;
+    console.log("Current User ID:", this.currentUserId);
+  }).catch(error => {
+    console.error("Error getting current user ID:", error);
+  });
+  this.userService.getUsers().subscribe(users => {
+    this.currentUser = users.find(user => user.id === this.currentUserId);
+    if (this.currentUser) {
+      this.user = this.currentUser.name;
+      this.currentUserAvatar = this.currentUser.avatar;
+    }
+  });
   this.subscription = this.threadsService.selectedThread.subscribe(thread => {
     if (thread) {
       this.selectedThread = thread;
-      console.log("selected thread in sidebar: ",this.selectedThread);
       this.threadMessages = this.selectedThread.messages;
-      console.log('messages in thread: ',this.selectedThread.messages);
     }
   });
 
@@ -67,6 +93,68 @@ ngOnInit(): void {
     this.allMessages = messages;
     });
 
+    this.subscription = this.userService.selectedUser.subscribe(user => {
+      this.selectedUser = user;
+      this.loadMessages();
+    });
+    
+}
+
+saveMessage(thread:any) {
+  if (this.message.body.trim() !== '') {
+    if (this.uploadedFileInfo) {
+      // If an image is uploaded, include its information in the message body
+      this.message.body = this.uploadedFileInfo.url;
+      this.uploadedFileInfo = null; // Reset uploaded file information
+    }
+
+    this.message.user = this.currentUserId;
+    //this.message.toUser = this.selectedUser.id;
+    this.message.createdAt = new Date();
+    this.messagesService.saveMessage(this.message).then(() => {
+      console.log('Message saved successfully');
+      console.log("Nachricht hinzugefügt: ",thread.messages); 
+      this.message.body = '';
+      setTimeout(() => this.scrollToBottom(), 0); // Scroll to bottom with a delay after saving a message
+      this.updatethreadMessages(thread, this.message);
+
+    }).catch(error => {
+      console.error('Error saving message:', error);
+    });
+  } else {
+    console.error('No current user ID or selected user available, or message is empty');
+  }
+}
+
+updatethreadMessages(thread:any, message:any){
+      const coll = doc(this.firestore, 'threads', thread.id);
+      //Add message ID to thread messages
+      updateDoc(coll, {messages: arrayUnion(message.id)}).then(r => console.log(r));
+    }
+
+loadMessages() {
+  this.messagesService.getMessages().subscribe(data => {
+    const allMessages = data.map(e => {
+      const payloadData = e.payload.doc.data() as any;
+      const message: Message = {
+        id: e.payload.doc.id,
+        ...payloadData
+      };
+      if (payloadData.createdAt && payloadData.createdAt.seconds) {
+        message.createdAt = new Date(payloadData.createdAt.seconds * 1000);
+      }
+      return message;
+    });
+
+  });
+}
+
+scrollToBottom(): void {
+  try {
+    if (this.myScrollContainer && this.myScrollContainer.nativeElement) {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    }
+  } catch(err) { }
 }
 
 selectCurrentThread(threadId:any){
@@ -78,10 +166,147 @@ else {
 }
 }
 
+selectCurrentThreadAuthor(userId:any, thread:any){
+  this.selectedThreadAuthorId = thread.authorId; 
+  if(userId === this.selectedThreadAuthorId){
+    return true;
+  }
+  else {
+    return false;
+  }
+  }
+
 messageSelectedToThread(messageId:any){
   let indexChecked = this.threadMessages.indexOf(messageId);
   return indexChecked > -1;
 }
 
+chosen:any
+  hideEmoticonMenu = signal<any | null>(null);
+  selectEmo(emoticon: string, messageId: string) {
+    this.showReactions = true;
+    this.chosen = emoticon;
+    const newReaction: Reaction = {
+        emoji: emoticon,
+        userId: this.currentUserId ?? ''
+    };
+    const messageToUpdate = this.messages.find(message => message.id === messageId);
+    if (messageToUpdate) {
+        if (!messageToUpdate.reactions) {
+            messageToUpdate.reactions = [newReaction];
+        } else {
+            const existingReactionIndex = messageToUpdate.reactions.findIndex(
+                reaction => reaction.userId === this.currentUserId && reaction.emoji === emoticon
+            );
+            if (existingReactionIndex !== -1) {
+                messageToUpdate.reactions.splice(existingReactionIndex, 1);
+            } else {
+                messageToUpdate.reactions.push(newReaction);
+            }
+        }
+        this.saveUpdatedMessage(messageToUpdate);
+    } else {
+        console.error("Nachricht nicht gefunden");
+    }
+}
+
+getReactionsSummary(message: Message): { emoji: string, count: number, userId?: string }[] {
+  const summary = new Map<string, { count: number, userId?: string }>();
+
+  message.reactions.forEach(reaction => {
+    const existingReaction = summary.get(reaction.emoji);
+    if (existingReaction) {
+      existingReaction.count += 1;
+    } else {
+      summary.set(reaction.emoji, { count: 1, userId: reaction.userId });
+    }
+  });
+
+  return Array.from(summary, ([emoji, { count, userId }]) => ({ emoji, count, userId }));
+}
+
+saveUpdatedMessage(message: Message) {
+  this.messagesService.updateMessage(message).then(() => {
+    console.log("Nachricht erfolgreich aktualisiert");
+  }).catch(error => {
+    console.error("Fehler beim Aktualisieren der Nachricht:", error);
+  });
+}
+
+showEmoticonMenu(i:number): void {
+  this.hoveredIndex=i;
+  this.changeDetector.detectChanges();
+}
+
+async upload(event:any){
+  const file = event.target.files[0];
+
+  if(file){
+    console.log(file);
+    const path=`messageImage/${file.name}`
+    const uploadTask=  await this.fireStorage.upload(path,file)
+    const url = await uploadTask.ref.getDownloadURL()
+    console.log(url);
+    this.uploadedFileInfo = {
+      name: file.name,
+      url: url
+    };
+    this.message.body= this.uploadedFileInfo.name
+  }
+}
+
+
+async uploadPDF(event: any) {
+  try {
+    const file = event.target.files[0];
+
+    if (file && file.type === 'application/pdf') {
+      const path = `messagePDF/${file.name}`;
+      const uploadTask = await this.fireStorage.upload(path, file);
+      const url = await uploadTask.ref.getDownloadURL();
+      console.log(url);
+
+      this.uploadedFileInfo = {
+        name: file.name,
+        url: url
+      };
+
+      this.message.body = this.uploadedFileInfo.name;
+      this.thread.title= this.uploadedFileInfo.name;
+    } else {
+      console.error('Invalid file format. Please select a PDF file.');
+    }
+  } catch (error) {
+    console.error('Error uploading PDF:', error);
+  }
+}
+
+isImage(url: string): boolean {
+  const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp'];
+  const isImage = imageExtensions.some(ext => url.toLowerCase().includes(ext));
+
+  return isImage;
+}
+
+isPDF(url: string): boolean {
+  const isPDF = url.toLowerCase().includes('.pdf');
+
+  return isPDF;
+}
+
+getPDFFileName(url: string): string {
+  const decodedUrl = decodeURIComponent(url);
+  const urlParts = decodedUrl.split('/');
+  return urlParts[urlParts.length - 1].split('?')[0];
+}
+
+  getUserName(userId: string | undefined): string {
+    const user = this.allUsers.find(user => user.id === userId);
+    return user ? user.name : '';
+  }
+
+  addEmoticon(emoticon: string) {
+    this.message.body = (this.message.body || '') + emoticon;
+  }
 
 }
